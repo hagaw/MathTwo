d7c0961e8761dcf52e96c60b7bc2c87d10ebb87a
ryanweaver718 <ryan.weaver718@gmail.com>
2018-09-24 11:25:16 -0400

    Two Math  project

---
+++ src/mathnew/Main.java
@@ -0,0 +1,19 @@
+package mathnew;
+
+public class Main {
+
+    public static void main(String[] args) {
+//Declarign the varible
+        int myNumber;
+        Double myotherNumber;
+        Double theAvrage;
+
+   // Assiging the variable
+   myNumber=2;
+   myotherNumber=1.7938;
+
+   System.out.println("My first number is "+ myNumber);
+   System.out.println("My other number is "+myotherNumber);
+	// write your code here
+    }
+}
